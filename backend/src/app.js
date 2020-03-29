a23f1be7280e9a3b26c4b1f8f8345de4d4d42af2
Bruno Nobrega <bruno.nobrega@hotmail.com>
2020-03-29 19:51:10 -0300

    Ajustando erro da lib cors -- comentei a linha par anao usa-la

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(cors({
 //     origin: 'http://meuapp.com'
 // }));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
